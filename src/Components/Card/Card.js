dc3f470b998951bb79458ecff95f5b9e89d96dea
Philipp Weiss <p.weiss85@gmx.de>
2022-09-20 09:50:06 +0200

    delete assets folder

--- src/Components/Card/Card.js
+++ src/Components/Card/Card.js
@@ -15,10 +15,8 @@ export function Card() {
         <path d="M17 3H7c-1.1 0-1.99.9-1.99 2L5 21l7-3 7 3V5c0-1.1-.9-2-2-2zm0 15l-5-2.18L7 18V5h10v13z" />
       </svg>
       <h2>What property flips the axes in flexbox?</h2>
-      <button data-js="showanswer-button" className="button__show-answer">
-        Show answer
-      </button>
-      <article data-js="answertext" className="article--hidden">
+      <button className="button__show-answer">Show answer</button>
+      <article className="article--hidden">
         <p className="quiz-card__answer">
           To flip the axes in a flexbox you need to you use the property 'flex-direction'.
         </p>
